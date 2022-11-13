d6a7f45a34bde170c1f0898efd8001549b117775
wakewakame <0214.taiki@gmail.com>
2022-11-13 23:48:59 +0900

    ローパスフィルタの実装

--- audio-processor.js
+++ audio-processor.js
@@ -4,19 +4,42 @@
 class AudioProcessor extends AudioWorkletProcessor {
   constructor(options) {
     super();
+    // 双二次フィルタに用いるパラメータの初期値
+    this.params = {
+      a0: 1.0, a1: 0.0, a2: 0.0,
+      b0: 1.0, b1: 0.0, b2: 0.0,
+    };
+    // ワーカーの生成元から送られてくるパラメータを適応
     this.port.onmessage = (e) => {
-      console.log(`audio-processor: ${e.data}`);
+      this.params = JSON.parse(e.data);
+    };
+    // 過去の入出力の値
+    this.val = {
+      in1: 0.0, in2: 0.0,
+      out1: 0.0, out2: 0.0,
     };
   }
   process(inputs, outputs, parameters) {
-    const array = (new Uint8Array([1,2,3,4]));
-    this.port.postMessage(array, [array.buffer]);
+    const val = this.val;
+    const params = this.params;
     const output = outputs[0];
     output.forEach((channel) => {
       for (let i = 0; i < channel.length; i++) {
-        channel[i] = (Math.random() * 2 - 1) * 0.0;
+        const in0 = (Math.random() * 2 - 1) * 0.1;
+        const out0 = in0      * params.b0/params.a0 +
+                     val.in1  * params.b1/params.a0 +
+                     val.in2  * params.b2/params.a0 -
+                     val.out1 * params.a1/params.a0 -
+                     val.out2 * params.a2/params.a0;
+        this.val.in2  = val.in1;
+        this.val.in1  = in0;
+        this.val.out2 = val.out1;
+        this.val.out1 = out0;
+        channel[i] = out0;
       }
     });
+    // 生成した波形をワーカーの生成もとに送る
+    //this.port.postMessage(output);
     return true;
   }
 }
--- index.js
+++ index.js
@@ -18,5 +18,20 @@ document.addEventListener("click", async (e) => {
     };
     return;
   }
-  port.postMessage(`mouse: ${e.pageX}, ${e.pageY}`);
+});
+document.addEventListener("mousemove", async (e) => {
+  const samplerate = 44100;  // サンプリング周波数
+  const freq = 1.0 + e.pageX * 10;  // カットオフ周波数
+  const q = 2.0;  // Q値
+  const omega = 2.0 * Math.PI * freq / samplerate;
+  const alpha = Math.sin(omega) / (2.0 * q);
+  const params = {
+    a0:  1.0 + alpha,
+    a1: -2.0 * Math.cos(omega),
+    a2:  1.0 - alpha,
+    b0: (1.0 - Math.cos(omega)) / 2.0,
+    b1:  1.0 - Math.cos(omega),
+    b2: (1.0 - Math.cos(omega)) / 2.0,
+  };
+  port?.postMessage(JSON.stringify(params));
 });
